9f4fb2a2b8767972e7a2aceed849a8d524520f00
James Young <hello@jamesyoung.ch>
2022-10-13 03:16:27 +0200

    feat: create add-nvmrc-file.sh

---
+++ add-nvmrc-file.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# Add a .nvmrc file to the current directory.
+echo "Adding .nvmrc file..."
+
+# get node version string, or use default "lts/gallium"
+NODE_V=${1:-"lts/gallium"}
+
+# write file
+echo "-> Writing file"
+echo $NODE_V > .nvmrc
+
+# create git commit
+git add package.json
+git commit -m "chore: add only-allow" --quiet &> /dev/null
+
+echo "...done!"
