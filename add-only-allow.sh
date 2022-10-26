79d4ae8afe208e95c50084701ec6ae2bb2999f96
James Young <hello@jamesyoung.ch>
2022-10-26 14:02:17 +0200

    feat: make git committing conditional in add-only-allow.sh

--- add-only-allow.sh
+++ add-only-allow.sh
@@ -2,6 +2,16 @@
 # Add only-allow to the Node project in the current directory.
 echo "Adding only-allow..."
 
+# Set command args
+GIT=false # don't commit to git by default
+
+# read args
+for ARG in $@; do
+  case $ARG in
+    --git) GIT=true;;
+  esac;
+done
+
 # get package manager name, or use default "npm"
 PM_NAME=${1:-"npm"}
 
@@ -9,8 +19,11 @@ PM_NAME=${1:-"npm"}
 echo "-> Writing config"
 npm pkg set "scripts.preinstall"="npx only-allow $PM_NAME"
 
-# create git commit
-git add package.json
-git commit -m "chore: add only-allow" --quiet &> /dev/null
+# (conditionally) create git commit
+if $GIT; then
+  echo "-> Committing file to Git"
+  git add package.json
+  git commit -m "chore: add only-allow" --quiet &> /dev/null
+fi
 
 echo "...done!"
