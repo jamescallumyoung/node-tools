fa207471b0553d7f2434fda748934323966075c5
James Young <hello@jamesyoung.ch>
2022-10-26 13:50:19 +0200

    feat: make git committing conditional in add-nvmrc-file.sh

--- add-nvmrc-file.sh
+++ add-nvmrc-file.sh
@@ -2,6 +2,16 @@
 # Add a .nvmrc file to the current directory.
 echo "Adding .nvmrc file..."
 
+# Set command args
+GIT=false # don't commit to git by default
+
+# read args
+for ARG in $@; do
+  case $ARG in
+    --git) GIT=true;;
+  esac;
+done
+
 # get node version string, or use default "lts/gallium"
 NODE_V=${1:-"lts/gallium"}
 
@@ -9,8 +19,11 @@ NODE_V=${1:-"lts/gallium"}
 echo "-> Writing file"
 echo $NODE_V > .nvmrc
 
-# create git commit
-git add .nvmrc
-git commit -m "chore: add only-allow" --quiet &> /dev/null
+# (conditionally) create git commit
+if $GIT; then
+  echo "-> Committing file to Git"
+  git add .nvmrc
+  git commit -m "chore: add only-allow" --quiet &> /dev/null
+fi
 
 echo "...done!"
